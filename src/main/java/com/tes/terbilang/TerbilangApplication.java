98051f145ac831e673021194bacefca1e757fe73
sendist <sendisetiawan10902@gmail.com>
2025-10-22 13:35:26 +0700

    first commit

---
+++ src/main/java/com/tes/terbilang/TerbilangApplication.java
@@ -0,0 +1,13 @@
+package com.tes.terbilang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerbilangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerbilangApplication.class, args);
+	}
+
+}
